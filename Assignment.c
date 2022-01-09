f3a356ed02b54ef7a11d7bbe3080672a57a2f6a5
kat077 <bhumikamagar13@gmail.com>
2022-01-09 22:46:48 +0545

    Addition feature added

---
+++ Assignment.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+
+    float x,y,sum;
+    printf("Enter the value of x:\n");
+    scanf("%f",&x);
+    printf("Enter the value of y:\n");
+    scanf("%f",&y);
+    sum= x+y;
+    printf ("\nThe sum of %f and %f is: %f\n",x,y,sum);
+
+}
