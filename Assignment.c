79192d616405a1be5fe688e0813c131169d50999
kat077 <bhumikamagar13@gmail.com>
2022-01-09 22:50:14 +0545

    Subtraction feature added

--- Assignment.c
+++ Assignment.c
@@ -2,12 +2,14 @@
 int main()
 {
 
-    float x,y,sum;
+    float x,y,sum,diff;
     printf("Enter the value of x:\n");
     scanf("%f",&x);
     printf("Enter the value of y:\n");
     scanf("%f",&y);
     sum= x+y;
     printf ("\nThe sum of %f and %f is: %f\n",x,y,sum);
+    diff=x-y;
+     printf ("\nThe difference between %f and %f is: %f\n",x,y,diff);
 
 }
